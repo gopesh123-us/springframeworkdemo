f2f82436983b7c9600d62536ef8787b47052a19f
Gopesh Sharma <gopesh.sharma@gmail.com>
2024-02-03 14:47:05 -0500

    modified config

--- src/main/java/demo/Doctor.java
+++ src/main/java/demo/Doctor.java
@@ -1,11 +1,26 @@
 package demo;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(scopeName = "prototype")
 public class Doctor implements Staff {
+
+	public void assist() {
+		System.out.println("Doctor is assisting!");
+	}
+	
 	
 	String qualification;
 	
-	public void assist() {
-		System.out.println("Doctor is assisting!");
+	public Doctor() {
+		
+	}
+
+	public Doctor(String theQualification) {
+		this.qualification = theQualification;
 	}
 
 	public String getQualification() {
@@ -14,6 +29,13 @@ public class Doctor implements Staff {
 
 	public void setQualification(String qualification) {
 		this.qualification = qualification;
-	}	
+	}
+
+	@Override
+	public String toString() {
+		return "Doctor [qualification=" + qualification + "]";
+	}
 	
+	
+
 }
--- src/main/java/demo/Main.java
+++ src/main/java/demo/Main.java
@@ -1,19 +1,29 @@
 package demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
 		Staff staffDoctor = context.getBean(Doctor.class);
 		Staff staffNurse = context.getBean(Nurse.class);
 		Doctor doctor = context.getBean(Doctor.class);
-		// Nurse nurse = (Nurse) context.getBean("nurse");//
+		Nurse nurse = (Nurse) context.getBean("nurse");//
 		staffDoctor.assist();
 		staffNurse.assist();
-		System.out.println(doctor.getQualification());
+		doctor.setQualification("MBBS");
+		System.out.println(doctor);
+
+		Doctor doctor1 = context.getBean(Doctor.class);
+		System.out.println("2nd time: " + doctor1);
+		/* System.out.println(doctor.getQualification()); */
+
 	}
 
 }
